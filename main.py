191784c04490a70ff8ffe51e9d7bb6e4ee07608c
Hardik Kundal <hardik@Hardiks-MacBook-Air.local>
2026-01-31 06:59:10 +0530

    post ml model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import json
 from pathlib import Path
+from ml.gp_model import train_model
 
 
 DATA_PATH = Path("data/training_data.json")
@@ -33,6 +34,11 @@ def view_data():
         "count":len(trial_data)
     }
 
+#connecting ml_model and fastapi
+@app.post("/api/model/train")
+def train_gp_model():
+    data = load_training_data()
+    trial_data = data.get("trials",[])
 
 
               
