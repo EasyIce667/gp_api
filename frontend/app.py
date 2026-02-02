62b503d683a0b91827492fb1d293cbc2d2b6871e
Hardik Kundal <hardik@Hardiks-MacBook-Air.local>
2026-02-02 09:35:35 +0530

    added streamlitupdate1

--- frontend/app.py
+++ frontend/app.py
@@ -9,3 +9,34 @@ st.set_page_config(
     page_icon="🧪",
     layout="wide"
 )
+
+# main title 
+st.title("🧪 Material Impact Strength Predictor")
+
+# side bar
+st.sidebar.title("Navigation")
+page = st.sidebar.radio(
+    "Go to",
+    ["View Training data","Train Model (Gaussian Process)","Predict Impact Strength"]
+
+)
+
+# view training data
+if page == "View Training data":
+    st.header("Training Dataset")
+
+    if st.button("Load Data"):
+        responce = requests.get(f"{API_BASE_URL}/api/data")
+
+        if responce.status_code == 200:
+            data = responce.json()
+            trial_data = data["trials"]
+
+            df = pd.json_normalize(trial_data)
+            st.dataframe(df, use_container_width=True)
+
+            st.success(f"Total Samples:{len(trial_data)}")
+        else:
+            st.error("Failed to Load data")
+
+
